1ba460f3a390a3cf9a728d6d38d8f43274b21462
Ewerton Augusto <ewerton.santos2210@gmail.com>
2021-03-29 20:16:10 -0300

    Ajustes falha AboutUs

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
-import store from './store';
-
-import Home from './components/Home';
-
 import './assets/standard.css'
 import 'react-toastify/dist/ReactToastify.css';
+
+import store from './store';
+import Home from './components/Home';
 import AboutUs from './components/AboutUs';
+
+
 
 
 const App: React.FC = () => {
